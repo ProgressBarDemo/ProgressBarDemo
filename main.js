d461ac778fa9fc8d45d0e4a652300bea955df459
willriggins <williamriggins2013@gmail.com>
2016-10-21 21:52:34 -0400

    fix formatting

--- main.js
+++ main.js
@@ -9,55 +9,53 @@ var prog = document.getElementById('prog'),
     j = 0;
 
 $('#reload').one("click", function() {
-  location.reload();
+    location.reload();
 })
 
-$('#download').one("click", function(){
+$('#download').one("click", function() {
 
-  $(this).addClass('disabled');
+    $(this).addClass('disabled');
 
-clearInterval(i);
-clearInterval(j);
-i = 0;
-j = 0;
-
-
-  for(; i <= 100; i++){
-      (function(i){
-          setTimeout(function(){
-              percent.innerHTML = i;
-              $('#prog').width(i + "%");
-
-              if (i >= 50 && i <= 99) {
-                $('#prog').removeClass('progress-bar-danger');
-                $('#prog').addClass('progress-bar-warning');
-              }
-              else if (i >99) {
-                $('#prog').removeClass('progress-bar-warning');
-                $('#prog').addClass('progress-bar-success');
-              }
-
-          }, time * i);
-      }(i));
-  }
-
-  for(; j <= 100; j++){
-      (function(j){
-          setTimeout(function(){
-              fiberPercent.innerHTML = j;
-              $('#fiber-prog').width(j + "%");
-
-              if (j >= 50 && j <= 99) {
-                $('#fiber-prog').removeClass('progress-bar-danger');
-                $('#fiber-prog').addClass('progress-bar-warning');
-              }
-              else if (j >99) {
-                $('#fiber-prog').removeClass('progress-bar-warning');
-                $('#fiber-prog').addClass('progress-bar-success');
-              }
+    clearInterval(i);
+    clearInterval(j);
+    i = 0;
+    j = 0;
 
 
-          }, time/multiplier * j);
-      }(j));
-  }
+    for (; i <= 100; i++) {
+        (function(i) {
+            setTimeout(function() {
+                percent.innerHTML = i;
+                $('#prog').width(i + "%");
+
+                if (i >= 50 && i <= 99) {
+                    $('#prog').removeClass('progress-bar-danger');
+                    $('#prog').addClass('progress-bar-warning');
+                } else if (i > 99) {
+                    $('#prog').removeClass('progress-bar-warning');
+                    $('#prog').addClass('progress-bar-success');
+                }
+
+            }, time * i);
+        }(i));
+    }
+
+    for (; j <= 100; j++) {
+        (function(j) {
+            setTimeout(function() {
+                fiberPercent.innerHTML = j;
+                $('#fiber-prog').width(j + "%");
+
+                if (j >= 50 && j <= 99) {
+                    $('#fiber-prog').removeClass('progress-bar-danger');
+                    $('#fiber-prog').addClass('progress-bar-warning');
+                } else if (j > 99) {
+                    $('#fiber-prog').removeClass('progress-bar-warning');
+                    $('#fiber-prog').addClass('progress-bar-success');
+                }
+
+
+            }, time / multiplier * j);
+        }(j));
+    }
 })
