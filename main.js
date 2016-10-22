5cc79b056ccd2507c8ec8e9b4b7fc2aa51d586b3
willriggins <williamriggins2013@gmail.com>
2016-10-21 21:19:52 -0400

    branded, buttons

--- main.js
+++ main.js
@@ -1,24 +1,63 @@
 var prog = document.getElementById('prog'),
     percent = document.getElementById('percent'),
     fiberProg = document.getElementById('fiber-prog'),
-    fiberPercent = document.getElementById('fiber-percent');
+    fiberPercent = document.getElementById('fiber-percent'),
+    time = 1000,
+    multiplier = 125,
+    filesizeGB,
+    i = 0,
+    j = 0;
 
-for(i = 1; i <= 100; i++){
-    (function(i){
-        setTimeout(function(){
-            percent.innerHTML = i;
-            $('#prog').width(i + "%");
+$('#reload').one("click", function() {
+  location.reload();
+})
 
-        }, 1000 * i);
-    }(i));
-}
+$('#download').one("click", function(){
 
-for(j = 1; j <= 100; j++){
-    (function(j){
-        setTimeout(function(){
-            fiberPercent.innerHTML = j;
-            $('#fiber-prog').width(j + "%");
+  $(this).addClass('disabled');
 
-        }, 8 * j);
-    }(j));
-}
+clearInterval(i);
+clearInterval(j);
+i = 0;
+j = 0;
+
+
+  for(; i <= 100; i++){
+      (function(i){
+          setTimeout(function(){
+              percent.innerHTML = i;
+              $('#prog').width(i + "%");
+
+          }, time * i);
+      }(i));
+  }
+
+  for(; j <= 100; j++){
+      (function(j){
+          setTimeout(function(){
+              fiberPercent.innerHTML = j;
+              $('#fiber-prog').width(j + "%");
+
+          }, time/multiplier * j);
+      }(j));
+  }
+})
+// for(i = 1; i <= 100; i++){
+//     (function(i){
+//         setTimeout(function(){
+//             percent.innerHTML = i;
+//             $('#prog').width(i + "%");
+//
+//         }, time * i);
+//     }(i));
+// }
+//
+// for(j = 1; j <= 100; j++){
+//     (function(j){
+//         setTimeout(function(){
+//             fiberPercent.innerHTML = j;
+//             $('#fiber-prog').width(j + "%");
+//
+//         }, time/multiplier * j);
+//     }(j));
+// }
